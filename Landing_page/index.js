e4240fc19bac358b4b39bd40cb1c705f0632e50d
Chhour Thaisan <sanezzy17@gmail.com>
2020-12-17 11:39:38 +0700

    test-02-v2

--- Landing_page/index.js
+++ Landing_page/index.js
@@ -1,17 +1,7 @@
-const menuBtn = document.querySelector(".c_menu__btn");
-const menuItems = document.querySelector(".c_header__menu");
-const menuItem = document.querySelectorAll(".c_header__menu li");
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100vw";
+}
 
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  menuItems.classList.toggle("open");
-
-  document.body.style.overflow = "hidden";
-});
-
-menuItem.forEach((item, index) => {
-  item.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
-});
-// menuItem.forEach((item, index) => {
-//   item.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 0.3}s`;
-// });
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
