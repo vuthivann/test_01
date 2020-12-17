2d30bc64cb9337ea59686b9e8bc368d0971227e5
Chhour Thaisan <sanezzy17@gmail.com>
2020-12-17 14:48:02 +0700

    testv2

--- Landing_page/index.js
+++ Landing_page/index.js
@@ -1,3 +1,8 @@
+const openMenu = document.querySelector('.openMenu')
+const sideNavHeader = document.querySelector('.sideNavHeader')
+const navMenu = document.querySelector('.navMenu')
+const sidenav = document.querySelector('.sidenav')
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "100vw";
 }
@@ -5,3 +10,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function myFunction(x) {
+  if (x.matches) {
+    openMenu.style.display = "";
+    sideNavHeader.style.display = "";
+    navMenu.style.display = "block";
+  } else {
+    openMenu.style.display = "none";
+    sideNavHeader.style.display = "none";
+    navMenu.style.display = "flex";
+  }
+}
+
+var x = window.matchMedia("(max-width: 768px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
