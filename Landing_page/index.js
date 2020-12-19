535a276340ed66b211d866467db1a390ef80279e
vuthivann <yong.vuthivann19@kit.edu.kh>
2020-12-19 12:19:10 +0700

    ddf

--- Landing_page/index.js
+++ Landing_page/index.js
@@ -1,14 +1,15 @@
-const openMenu = document.querySelector('.openMenu')
-const sideNavHeader = document.querySelector('.sideNavHeader')
-const navMenu = document.querySelector('.navMenu')
-const sidenav = document.querySelector('.sidenav')
+// section 1
+const openMenu = document.querySelector(".c_openMenu");
+const sideNavHeader = document.querySelector(".c_sideNavHeader");
+const navMenu = document.querySelector(".c_navMenu");
+const sidenav = document.querySelector(".c_sidenav");
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100vw";
+  document.getElementById("c_mySidenav").style.width = "100vw";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("c_mySidenav").style.width = "0";
 }
 function myFunction(x) {
   if (x.matches) {
@@ -22,6 +23,25 @@ function myFunction(x) {
   }
 }
 
-var x = window.matchMedia("(max-width: 768px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var x = window.matchMedia("(max-width: 1024px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
+
+const debounce = (fn) => {
+  let frame;
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+};
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
+
+storeScroll();
